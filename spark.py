45a9d3859271dc5a4255a0d5a88a4354d9727560
asifanas <asifanas00786@gmail.com>
2024-02-13 23:08:10 +0530

    python code has been added

--- spark.py
+++ spark.py
@@ -4,6 +4,6 @@ if __name__=="__main__":
 
     spark = SparkSession.builder.appName("spark").master("local[2]").getOrCreate()
 
-    df = spark.read.format("csv").option("header", "true").option("inferschema", "true").load("data\Fire_Department_Calls_for_Service.csv")
+    df = spark.read.format("csv").option("header", "true").option("inferschema", "true").load("C:\projects\pycharm\data\Fire_Department_Calls_for_Service.csv")
 
-    df.count()
+    df.printSchema()
