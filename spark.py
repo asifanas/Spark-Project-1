6c7413a208e099bdbc0164d6ef91a55b2536c29c
Asif Anas <54286844+asifanas@users.noreply.github.com>
2024-02-18 16:26:06 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -22,7 +22,7 @@ if __name__=="__main__":
 
     # Question 2: Delay type more than 5 sec
     delay_time = spark.sql("""
-    SELECT 
+    SELECT
         `Call Number`,
         `Received DtTm`,
         `Response DtTm`,
@@ -40,20 +40,47 @@ if __name__=="__main__":
 
     # Question 3: Most Common Call Types
 
-    most_common_call = spark.sql("""SELECT `Call type`, COUNT(*) as total_call 
+    most_common_call = spark.sql("""SELECT `Call type`, COUNT(*) as total_call
                                  FROM fire_department GROUP BY 1 ORDER BY 2 DESC LIMIT 1""")
 
     most_common_call.show()
 
-    # Question 4: Zip Codes for Most Common Calls
-    zip_code = spark.sql("""SELECT `Zipcode of Incident` as zip_code FROM 
+    # # Question 4: Zip Codes for Most Common Calls
+    zip_code = spark.sql("""SELECT `Zipcode of Incident` as zip_code FROM
                         fire_department WHERE `Call type` = (SELECT `Call type`
-                        FROM (SELECT `Call type`, COUNT(*) as total_call 
+                        FROM (SELECT `Call type`, COUNT(*) as total_call
                         FROM fire_department GROUP BY 1 ORDER BY 2 DESC LIMIT 1) cust) LIMIT 1""")
 
     zip_code.show()
 
     #Question 5: San Francisco Neighborhoods in Zip Codes 94102 and 94103 trying to solve using pyspark sql
+
+    #Question 8: Distinct Years of Data
+
+    Distinct_year = spark.sql("""SELECT YEAR(data_loaded_at) AS year, COUNT(*) AS count
+    FROM fire_department
+    GROUP BY YEAR(data_loaded_at)
+    ORDER BY year""")
+
+    Distinct_year.show(20, truncate=False)
+
+    #Question 9: Week of the Year with Most Fire Calls in 2018
+
+    week_year = spark.sql("""
+    SELECT WEEKOFYEAR(`call date`) AS week_of_year, COUNT(*) AS call_count
+    FROM fire_department
+    WHERE to_date(`call date`, 'MM/dd/yyyy') BETWEEN '2018-01-01' AND '2018-12-31'
+    GROUP BY WEEKOFYEAR(`call date`)
+    ORDER BY call_count DESC
+    """)
+
+    week_year.show(10, truncate=False)
+
+
+
+
+
+
 
 
 
